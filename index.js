5581c3aa0897832d11785ba7fd61866cefbdf8ff
Julian Lywood-Mulcock <9203060+algorox@users.noreply.github.com>
2022-12-05 12:28:57 +0000

    rounded build

--- index.js
+++ index.js
@@ -234,39 +234,45 @@ router.get("/config_migrated", ensureAuthenticated(), async (req, res, next) => 
 });
 
 router.post("/migrate_config", ensureAuthenticated(), async (req, res, next) => {
-    console.log("Migrate Config request received.")
+    //console.log("Migrate Config request received.")
     console.log(JSON.stringify(req.body))
     var from_config;
     var to_config;
-    // from_config ={
-    //     AUTH0_DOMAIN: req.body.from_domain,
-    //     AUTH0_CLIENT_SECRET: req.body.from_secret,
-    //     AUTH0_CLIENT_ID: req.body.from_client,
-    //     AUTH0_ALLOW_DELETE: false
-    // }
 
-    // from_config ={
-    //     AUTH0_DOMAIN: req.body.to_domain,
-    //     AUTH0_CLIENT_SECRET: req.body.to_secret,
-    //     AUTH0_CLIENT_ID: req.body.to_client,
-    //     AUTH0_ALLOW_DELETE: false
-    // }
-
-
-    from_config = {
-        AUTH0_DOMAIN: process.env.FROM_DOMAIN,
-        AUTH0_CLIENT_SECRET: process.env.FROM_CLIENT_SECRET,
-        AUTH0_CLIENT_ID: process.env.FROM_CLIENT_ID,
+    from_config ={
+        AUTH0_DOMAIN: req.body.from_domain,
+        AUTH0_CLIENT_SECRET: req.body.from_secret,
+        AUTH0_CLIENT_ID: req.body.from_client,
         AUTH0_ALLOW_DELETE: false
     }
 
-    to_config = {
-        AUTH0_DOMAIN: process.env.TO_DOMAIN,
-        AUTH0_CLIENT_SECRET: process.env.TO_CLIENT_SECRET,
-        AUTH0_CLIENT_ID: process.env.TO_CLIENT_ID,
-        AUTH0_ALLOW_DELETE: false
+    to_config ={
+        AUTH0_DOMAIN: req.body.to_domain,
+        AUTH0_CLIENT_SECRET: req.body.to_secret,
+        AUTH0_CLIENT_ID: req.body.to_client,
+        AUTH0_ALLOW_DELETE: false,
+        AUTH0_EXCLUDED_CLIENTS: ['Auth0 Dashboard Backend Management Client'],
+        AUTH0_EXCLUDED: ['tenant'],
     }
 
+
+    // from_config = {
+    //     AUTH0_DOMAIN: process.env.FROM_DOMAIN,
+    //     AUTH0_CLIENT_SECRET: process.env.FROM_CLIENT_SECRET,
+    //     AUTH0_CLIENT_ID: process.env.FROM_CLIENT_ID,
+    //     AUTH0_ALLOW_DELETE: false
+    // }
+
+    // to_config = {
+    //     AUTH0_DOMAIN: process.env.TO_DOMAIN,
+    //     AUTH0_CLIENT_SECRET: process.env.TO_CLIENT_SECRET,
+    //     AUTH0_CLIENT_ID: process.env.TO_CLIENT_ID,
+    //     AUTH0_ALLOW_DELETE: false,
+    //     AUTH0_EXCLUDED_CLIENTS: ['Auth0 Dashboard Backend Management Client'],
+    //     AUTH0_EXCLUDED: ['tenant'],
+    // }
+
+    // https://github.com/auth0/auth0-deploy-cli/blob/master/docs/excluding-from-management.md
 
     fs.mkdtemp(path.join(os.tmpdir(), 'tenant-config-'), (err, folder) => {
         if (err) throw err;
@@ -284,20 +290,23 @@ router.post("/migrate_config", ensureAuthenticated(), async (req, res, next) => 
                     config: to_config,   // Option to sent in json as object
                 })
             })
-                    .then((output) => {
-                        res.status(200)
-                        res.send(
-                            {   
-                                'output': output,
-                                'tenant_migrated_from': from_config.AUTH0_DOMAIN,
-                                'tenant_migrated_to': to_config.AUTH0_DOMAIN
-                            })})
-                    .catch((error) => {
-                        res.status(400)
-                        res.send({ error: error })})
+            .then((output) => {
+                res.status(200)
+                res.send(
+                    {
+                        'output': output,
+                        'tenant_migrated_from': from_config.AUTH0_DOMAIN,
+                        'tenant_migrated_to': to_config.AUTH0_DOMAIN
+                    })
+            })
             .catch((error) => {
                 res.status(400)
-                res.send({ error: error })})
+                res.send({ error: error })
+            })
+            .catch((error) => {
+                res.status(400)
+                res.send({ error: error })
+            })
     });
 })
 
